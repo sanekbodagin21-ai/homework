ac4127f1be51533290fe601c646b85bc7e00eca9
Ekaterina <sanekbodagin21@gmail.com>
2026-04-20 01:04:37 +0300

    JShw8

---
+++ JShw8/js/main.js
@@ -0,0 +1,97 @@
+"use strict"
+
+const todoKeys = {
+    id: "id",
+    text: "text",
+    year: "year",
+    is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = todoId => `Todo with ${todoId} not found`
+const currentYear = new Date().getFullYear();
+
+const getNewTodoId = (todos) => {
+    return todos.reduce( (maxId, todo) =>  Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+};
+
+const createTodo = (todos, text, year ) => {
+    const newTodo = {
+        [todoKeys.id]: getNewTodoId(todos),
+        [todoKeys.text]: text, 
+        [todoKeys.is_completed]: false,
+        [todoKeys.year]: year,
+    };
+    todos.push(newTodo);
+    return newTodo;
+};
+
+const changeTodoStyleById = (todos, todoId ) => {
+    const todo = todos.find( todo => todo[todoKeys.id] === todoId);
+    if (!todo) {
+        console.error(errTodoNotFound(todoId));
+        return null;
+    } else {
+    todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+    return todo;
+    };
+};
+
+const deleteTodoById = (todos, todoId) => {
+    const todoIndex = todos.findIndex( (todo) => todo[todoKeys.id] === todoId)
+    if (todoIndex === -1) {
+        console.error(errTodoNotFound(todoId));
+        return todos;
+    } else {
+        todos.splice(todoIndex, 1);
+        return todos;
+    }
+};
+
+const isOverdue = todo => todo[todoKeys.year] < currentYear 
+
+const deleteOverdueTodos = todos => {
+    const initialLength = todos.length;
+    for (let i = todos.length - 1; i >= 0; i--) {
+        const todo = todos[i];
+        if (isOverdue(todo)) {
+            deleteTodoById(todos, todo[todoKeys.id]);
+        }
+    }
+    const deletedTodo = initialLength - todos.length;
+    if (deletedTodo > 0) {
+        console.log(`Removed ${deletedTodo} overdue tasks`)
+    } else {
+        console.log(`There are no overdue tasks!`)
+    }
+    return todos;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todos = document.querySelector(".todos");
+
+const createTodoElement = (text, year) => {
+    const list = document.createElement("li");
+    list.classList.add("todo");
+    list.innerHTML = `
+    <div class="todos-text">${text}</div>
+    <p class="todos-year">${year}</p>
+    <div class="todos-actions">
+            <button class="button-complete button">&#00001;</button>
+            <button class="button-delete button">&#00001;</button>
+    </div>
+    `;
+    return list;
+}
+
+    const handleCreateTodo = (todos, text) => {
+        const year = new Date().getFullYear();
+        const newTodo = createTodo(todos, text, year);
+        const todoElement = createTodoElement(newTodo[todoKeys.text], newTodo[todoKeys.year]);
+        document.querySelector(".todos").append(todoElement);
+        return newTodo;
+};
